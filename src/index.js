00999486c689471fd8990a1258261d92b3252df8
alessio <yellowaxe.az@gmail.com>
2018-04-24 17:51:19 +0200

    good positions

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-/* eslint-disable no-undef */
+/* eslint-disable no-undef,no-mixed-operators,no-console */
 const d3 = require('d3');
 require('./styles/nightsky.css');
 
 const startPath = 'M 152.000 172.000 L 175.511 184.361 L 171.021 158.180 L 190.042 139.639 L 163.756 135.820 L 152.000 112.000 L 140.244 135.820 L 113.958 139.639 L 132.979 158.180 L 128.489 184.361 L 152.000 172.000';
 
-const randomStars = Array(100).fill(1).map(() => ({
-  x: Math.random() * window.innerWidth,
-  y: Math.random() * window.innerHeight,
+const randomStars = Array(1).fill(1).map(() => ({
+  //  (window.innerWidth-200, -100) prevents the star from goind outside (right, left) the screen
+  x: Math.random() * (window.innerWidth - 100 + 1) - 100, // -200+100 to include negative numbers
+  y: Math.random() * (window.innerHeight - 100 + 1) - 100,
   rotate: Math.random() * 360,
-  scale: Math.random() + 0.5,
+  scale: 1, // Math.random() + 0.5,
   color: '#d0d0d0',
 }));
+
+// rotate: Math.random() * 0,
+console.log('window.innerWidth: ', window.innerWidth, 'window.innerHeight', window.innerHeight);
+console.log('random window.innerXXX:', randomStars[0]);
+console.log('window.innerWidth: ', window.innerWidth, 'window.innerHeight', window.innerHeight);
 
 d3.select('#root')
   .append('svg')
@@ -23,5 +29,5 @@ d3.select('#root')
   .attr('fill', randomStar => randomStar.color)
   .attr('stroke-width', 1.5)
   .attr('stroke', '#99ccff')
-  .attr('transform', randomStar => `scale(${randomStar.scale}) rotate(${randomStar.rotate}) translate(${randomStar.x} ${randomStar.y})`);
+  .attr('transform', randomStar => `scale(${randomStar.scale}) translate(${randomStar.x} ${randomStar.y})`);
 
