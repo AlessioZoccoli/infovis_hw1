04e5cec462726b1938a71f985ae6d34026bb6543
alessio <yellowaxe.az@gmail.com>
2018-04-26 11:59:37 +0200

    rotation, better randomization

--- src/index.js
+++ src/index.js
@@ -1,34 +1,50 @@
-/* eslint-disable no-undef,no-mixed-operators,no-console */
+/* eslint-disable no-undef,no-mixed-operators,no-console,no-restricted-globals,no-underscore-dangle,no-plusplus,max-len,prefer-const,no-trailing-spaces */
+
 const d3 = require('d3');
-require('./styles/nightsky.css');
+require('./styles/sky.css');
 
-const startPath = 'M 152.000 172.000 L 175.511 184.361 L 171.021 158.180 L 190.042 139.639 L 163.756 135.820 L 152.000 112.000 L 140.244 135.820 L 113.958 139.639 L 132.979 158.180 L 128.489 184.361 L 152.000 172.000';
+// SVG star path
+const starPath = 'M 0.000 20.000 L 23.511 32.361 L 19.021 6.180 L 38.042 -12.361 L 11.756 -16.180 L 0.000 -40.000 L -11.756 -16.180 L -38.042 -12.361 L -19.021 6.180 L -23.511 32.361 L 0.000 20.000';
+// pick a color
+const colors = c => d3.schemeSet3[c]; // const colors = t => d3.interpolatePiYG(t);
 
-const randomStars = Array(500).fill(1).map(() => ({
-  //  (window.innerWidth-200, -100) prevents the star from goind outside (right, left) the screen
-  // -200+100 = -100 to include negative numbers
-  x: Math.random() * (window.innerWidth - 100 + 1) - 100,
-  y: Math.random() * (window.innerHeight - 100 + 1) - 100,
-  rotate: Math.random() * 360,
-  scale: Math.random() * 0.7 + 0.3,
-  color: '#d0d0d0',
-}));
-
-// rotate: Math.random() * 0,
-console.log('window.innerWidth: ', window.innerWidth, 'window.innerHeight', window.innerHeight);
-console.log(randomStars[0]);
-console.log('window.innerWidth: ', window.innerWidth, 'window.innerHeight', window.innerHeight);
-
-d3.select('#root')
+const container = d3.select('#root')
   .append('svg')
-  .attr('class', 'container')
-  .selectAll('path')
-  .data(randomStars)
-  .enter()
-  .append('path')
-  .attr('d', startPath)
-  .attr('fill', randomStar => randomStar.color)
-  .attr('stroke-width', 1.5)
-  .attr('stroke', '#99ccff')
-  .attr('transform', randomStar => `scale(${randomStar.scale}) translate(${randomStar.x} ${randomStar.y})`);
+  .attr('class', 'container');
+
+// OFFSET prevents the stars from going outside the screen
+const getRandomStar = () => {
+  const OFFSET = 38;
+  let randX = OFFSET + (Math.random() * (self.innerWidth - OFFSET * 2));
+  let randY = OFFSET + (Math.random() * (self.innerHeight - OFFSET * 2));
+  let scale = Math.random() * 0.7 + 0.3;
+  let rotate = Math.random() * 360;
+  return [randX, randY, scale, rotate];
+};
+
+let data = [];
+
+/**
+ * (self.innerWidth-200, -100 +1) prevents the star from getting placed outside (right, left) the screen,
+ * +1 for max inclusion
+ * -200+100 = -100 to include negative numbers
+ */
+d3.interval(() => {
+  data = [...data, getRandomStar()];
+
+  container
+    .selectAll('path')
+    .data(data)
+    .enter()
+    .append('path')
+    .attr('d', starPath)
+    .attr('fill', colors(Math.floor(Math.random() * 12)))
+    .attr('stroke-width', 1.5)
+    .attr('stroke', '#99ccff')
+    .attr('transform', ([randX, randY, scale, rotate]) => `rotate(${rotate} ${randX} ${randY}) translate(${randX} ${randY}) scale(${scale})`);
+
+  container
+    .exit()
+    .remove();
+}, 1000);
 
